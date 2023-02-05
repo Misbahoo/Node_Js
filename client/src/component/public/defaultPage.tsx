6da23ec1ca7e8fdd6e858ce12ab09a9604706f5d
Misbahu Abubakar <miibahu@gmail.com>
2023-02-05 16:44:49 +0100

    Default page updated

--- client/src/component/public/defaultPage.tsx
+++ client/src/component/public/defaultPage.tsx
@@ -2,7 +2,11 @@ const DefaultPage = () => {
   return (
     <>
       <div className="bg-gray-200 mx-auto my-10 w-3/4 p-10 shadow-sm shadow-gray-600 rounded-lg">
-        <p className="text-center">Welcome</p>
+        <p className="text-center text-3xl text-green-600">Welcome</p>
+        <p className="text-center mt-5">
+          Click any of the subjects on the left to start the exam. Your time has
+          already started.
+        </p>
       </div>{" "}
     </>
   );
